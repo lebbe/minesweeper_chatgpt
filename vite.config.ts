f38093ce0718f841e4967a83a09fa53485bf4c08
Lebchen <lebchen@gmail.com>
2024-01-30 21:57:40 +0100

    Add docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
+  base: 'minesweeper_chatgpt',
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
